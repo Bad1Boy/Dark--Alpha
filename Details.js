70ff5f9ab6ea1b585c7e795e5167e6b047abb8ce
Bad1Boy <108726004+Bad1Boy@users.noreply.github.com>
2022-07-17 07:43:18 +0100

    Expect this project in the future

---
+++ Details.js
@@ -0,0 +1,13 @@
+/*
+Dark-Alpha Md Whatsapp Bot
+Telegram: https://t.me/RavinduManoj
+Facebook: https://www.facebook.com/ravindu.manoj.79
+Youtube: https://youtube.com/c/TechToFuture
+Coded By Ravindu Manoj and Sanuka Nimsath
+*/
+module.export = {
+	name: 'Dark-Alpha Multi-device Whatsapp Bot',
+	version: require('./package.json').version,
+	branch: 'master',
+	author: require('./package.json').author
+}
